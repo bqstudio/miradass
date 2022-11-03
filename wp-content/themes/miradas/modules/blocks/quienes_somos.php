4382781e81ff4d96aa10b8f9398163db5ac94171
Servidor <info@bq-studio.com>
2022-11-03 12:42:20 -0300

    php css

---
+++ wp-content/themes/miradas/modules/blocks/quienes_somos.php
@@ -0,0 +1,11 @@
+<section class="quienes_somos">
+    <div class="container">
+        <div class="quienes_somos__image">
+            <?php get_template_part('modules/components/image',NULL,array('image' => get_sub_field('image')) ); ?>
+        </div>
+        <div class="quienes_somos__cont">
+            <?php echo ($title = get_sub_field('title'))? '<div class="quienes_somos__title">'.$title.'</div>':'';
+            echo ($text = get_sub_field('text'))? '<p class="quienes_somos__text">'.$text.'</p>':'';  ?>
+        </div>
+    </div>
+</section>
