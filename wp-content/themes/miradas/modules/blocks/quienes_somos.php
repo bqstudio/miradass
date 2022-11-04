87d58e8fee9562ab97cadc501473659356df6a69
Servidor <info@bq-studio.com>
2022-11-03 21:37:19 -0300

    quienes somos mas bd

--- wp-content/themes/miradas/modules/blocks/quienes_somos.php
+++ wp-content/themes/miradas/modules/blocks/quienes_somos.php
@@ -1,9 +1,9 @@
 <section class="quienes_somos">
-    <div class="container">
-        <div class="quienes_somos__image">
-            <?php get_template_part('modules/components/image',NULL,array('image' => get_sub_field('image')) ); ?>
-        </div>
-        <div class="quienes_somos__cont">
+    <div class="quienes_somos__image">
+        <?php get_template_part('modules/components/image',NULL,array('image' => get_sub_field('image')) ); ?>
+    </div>
+    <div class="quienes_somos__cont">
+        <div class="container">
             <?php echo ($title = get_sub_field('title'))? '<div class="quienes_somos__title">'.$title.'</div>':'';
             echo ($text = get_sub_field('text'))? '<p class="quienes_somos__text">'.$text.'</p>':'';  ?>
         </div>
