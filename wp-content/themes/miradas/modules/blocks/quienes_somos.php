5568c903c07c04da4b19e5223226d4bb581b28fa
Enrique Scapoli <88351578+EnriqueHS@users.noreply.github.com>
2022-11-09 15:50:35 -0300

    ajustes

--- wp-content/themes/miradas/modules/blocks/quienes_somos.php
+++ wp-content/themes/miradas/modules/blocks/quienes_somos.php
@@ -2,9 +2,9 @@
     <div class="quienes_somos__image">
         <?php get_template_part('modules/components/image',NULL,array('image' => get_sub_field('image')) ); ?>
     </div>
-    <div class="quienes_somos__cont">
+    <div class="quienes_somos__cont" data-aos="fade-right">
         <div class="container">
-            <?php echo ($title = get_sub_field('title'))? '<div class="quienes_somos__title">'.$title.'</div>':'';
+            <?php echo ($title = get_sub_field('title'))? '<h2 class="quienes_somos__title">'.$title.'</h2>':'';
             echo ($text = get_sub_field('text'))? '<p class="quienes_somos__text">'.$text.'</p>':'';  ?>
         </div>
     </div>
