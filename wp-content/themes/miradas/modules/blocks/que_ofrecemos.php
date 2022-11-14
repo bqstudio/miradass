b6abd29014fd469f184b63c2ddf488d929ff20d1
Servidor <info@bq-studio.com>
2022-11-14 17:22:36 -0300

    php

--- wp-content/themes/miradas/modules/blocks/que_ofrecemos.php
+++ wp-content/themes/miradas/modules/blocks/que_ofrecemos.php
@@ -7,7 +7,7 @@
           <?php while( have_rows('que_ofrecemos') ): the_row();?>
             <div class="que_ofrecemos__item">
               <figure>
-                <?php get_template_part('modules/components/image',NULL,array('image' => get_sub_field('image')) ); ?>
+                <?php get_template_part('modules/components/image',NULL,array('image' => get_sub_field('image_background')) ); ?>
               </figure>
               <div class="overlay-text">
                 <?php echo ($title = get_sub_field('title'))? '<h3 class="que_ofrecemos__title">'.$title.'</h3>':''; ?>
