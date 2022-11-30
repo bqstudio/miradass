d4bb068f627d5a583b990b4cf1043b9c3365045a
Servidor <info@bq-studio.com>
2022-11-30 18:43:09 -0300

    team

---
+++ wp-content/themes/miradas/modules/blocks/nuestro_equipo.php
@@ -0,0 +1,13 @@
+<section class="nuestro_equipo">
+    <div class="container">
+        <div class="title">Nuestro Equipo</div>
+        <div class="nuestro_equipo__subtitle">Lorem ipsum dolor bla blqa bla</div>
+
+        <div class="nuestro_equipo__cont">
+            <div class="nuestro_equipo__item">
+                <img src="" alt="">
+                <div class="nuestro_equipo__persona">Fernanda Espinoza</div>
+            </div>
+        </div>
+    </div>
+</section>
