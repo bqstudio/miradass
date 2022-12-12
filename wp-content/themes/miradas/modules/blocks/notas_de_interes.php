f9cb602b319f49996c3a9572b1f5bd79a3c6aaed
Enrique Scapoli <88351578+EnriqueHS@users.noreply.github.com>
2022-12-12 11:52:01 -0300

    fix_notas

--- wp-content/themes/miradas/modules/blocks/notas_de_interes.php
+++ wp-content/themes/miradas/modules/blocks/notas_de_interes.php
@@ -1,21 +1,26 @@
-<?php if( have_rows('notas_de_interes') ): ?>
+
   <section class="notas_de_interes"> 
     <div class="container_big">
-      <div class="notas_de_interes__cont">
-        <?php while( have_rows('notas_de_interes') ): the_row(); ?>
-          <div class="notas_de_interes__item">
-            <div class="notas_de_interes__img">
-              <?php get_template_part('modules/components/image',NULL,array('image' => get_sub_field('image')) ); ?>
+    <?php echo ($title_notas_de_interes = get_sub_field('title_notas_de_interes'))? '<h3 class="title">'.$title_notas_de_interes.'</h3>':''?>
+
+      <?php if( have_rows('notas_de_interes') ): ?>
+        <div class="notas_de_interes__cont">
+          <?php while( have_rows('notas_de_interes') ): the_row(); ?>
+            <div class="notas_de_interes__item">
+              <div class="notas_de_interes__img">
+                <?php get_template_part('modules/components/image',NULL,array('image' => get_sub_field('image')) ); ?>
+              </div>
+              <?php echo ($title = get_sub_field('title'))? '<h4 class="notas_de_interes__title">'.$title.'</h4>':''?>
+              <?php echo ($description = get_sub_field('description'))? '<div class="notas_de_interes__description">'.$description.'</div>':''; ?>
+              <?php echo ($fuente = get_sub_field('fuente'))? '<div class="fuente">'.$fuente.'</div>':''?>
+              <?php if( $link = get_sub_field('link')): ?>
+                <a class="read_more" href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a> <!-- TODO: es necesario hacer un if? en este caso que va un link solo -->
+              <?php endif; ?>
             </div>
-            <?php echo ($title = get_sub_field('title'))? '<h4 class="notas_de_interes__title">'.$title.'</h4>':''?>
-            <?php echo ($description = get_sub_field('description'))? '<div class="notas_de_interes__description">'.$description.'</div>':''; ?>
-            <?php echo ($fuente = get_sub_field('fuente'))? '<div class="fuente">'.$fuente.'</div>':''?>
-            <?php if( $link = get_sub_field('link')): ?>
-              <a class="read_more" href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a> <!-- TODO: es necesario hacer un if? en este caso que va un link solo -->
-            <?php endif; ?>
-          </div>
-        <?php endwhile; ?>
-      </div>
+          <?php endwhile; ?>
+        </div>
+        <?php endif; ?>
     </div>
   </section>
-<?php endif; ?>
+
+
