095bec2a71033359b45cef389b8210f750c5fed5
Servidor <info@bq-studio.com>
2022-12-22 09:15:05 -0300

    php

--- wp-content/themes/miradas/modules/blocks/instagram.php
+++ wp-content/themes/miradas/modules/blocks/instagram.php
@@ -4,7 +4,7 @@
   <div class="container">
     <div class="layout_instagram__title">
       <?php if( $link = get_sub_field('link')): ?>
-          <a class="title" href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><?php echo $button['title']; ?></a>
+          <a class="title" href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a>
       <?php endif; ?>
     </div>  
     <?php if ($shortcode = get_sub_field('shortcode')): ?>
