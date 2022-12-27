5a31e3d7df2bbf1831dfeea19aaee10ce953faf6
Enrique Scapoli <88351578+EnriqueHS@users.noreply.github.com>
2022-12-27 19:55:18 -0300

    talleres-para-dinamizar

---
+++ wp-content/themes/miradas/modules/blocks/talleres.php
@@ -0,0 +1,16 @@
+<section class="talleres"> <!-- TODO: Dinamizar con un repeater...-->
+  <div class="container_big">
+    <div class="talleres__item">
+      <div class="talleres__imagen">
+      <img src="<?php echo get_template_directory_uri();?>/images/about-texture.png" alt="">
+      </div>
+      <div class="talleres__content">
+        <h3 class="h3">Titulo del taller</h3>
+        <div class="talleres__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis cum inventore ea tenetur odio, rerum tempora voluptatibus, numquam unde nesciunt aut delectus animi dolorum? Necessitatibus.
+        </div>
+        <div class="talleres__email">info@miradas.com.ar</div>
+      </div>
+    </div>
+  </div>
+</section>
