1f734f34b3256b9c362da7a386cfd4eda5b76dfa
bqstudio <enriquescapoli@gmail.com>
2023-06-08 12:48:34 -0300

    ajustes-finales-1

--- wp-content/themes/miradas/footer.php
+++ wp-content/themes/miradas/footer.php
@@ -25,13 +25,13 @@
 			<?php if( have_rows('bottom_links', 'options') ): ?>
 				<div class="bottom_links">
 					<?php while( have_rows('bottom_links', 'options') ): the_row(); 
-						 $link = get_sub_field('link') ?>
-							<a class="bottom_links__item" href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a>
+						$link = get_sub_field('link') ?>
+						<a class="bottom_links__item" href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a>
 					<?php endwhile; ?>
 				</div>
 			<?php endif; ?>
 			<div class="site-footer__bq">
-				By <a href="https://bq-studio.com/">BQ-Studio</a>
+				By <a href="https://bq-studio.com/" target="_blank" tabindex="0">BQ-Studio</a>
 			</div>
 		</div>
 	</footer><!-- #colophon -->
