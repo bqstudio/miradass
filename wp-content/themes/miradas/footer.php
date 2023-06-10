6f3e5fe5f689ad84334c44d4ffa99f6529cad826
bqstudio <info@bq-studio.com>
2023-06-10 00:06:36 -0300

    whatsapp

--- wp-content/themes/miradas/footer.php
+++ wp-content/themes/miradas/footer.php
@@ -39,6 +39,9 @@
 
 </div><!-- #page -->
 
+<?php if($whatsapp = get_field('whatsapp','option')): ?>
+	<a class="btn-whatsapp" href="https://api.whatsapp.com/send?phone=<?php echo $whatsapp; ?>&amp;text=Hola%20me%20estoy%20contactando%20desde%20el%20sitio%20web.&amp;source=&amp;data=" target="_blank"><i class="fab fa-whatsapp"></i></a>
+<?php endif; ?>
 <?php wp_footer(); ?>
 
 </body>
