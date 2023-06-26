337ab1fb183bbcaff79a7d602e5395adb019d11f
bqstudio <info@bq-studio.com>
2023-06-25 22:55:45 -0300

    js

--- wp-content/themes/miradas/js/lib/global.js
+++ wp-content/themes/miradas/js/lib/global.js
@@ -68,8 +68,4 @@ jQuery(document).ready(function($){
 	$(document).on('click', 'a[href^="#"]', function (event) {
 		event.preventDefault();
 	});
-
-	$('html, body').animate({
-		scrollTop: $($.attr(this, 'href')).offset().top
-	}, 500)
 });
